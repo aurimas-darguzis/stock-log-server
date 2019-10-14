197b298ba4c32351960c6f8de5ae7fd419b55b2a
aurimas-darguzis <aurimas@darguzis.com>
2019-10-14 13:48:23 +0100

    feat: connected to mongo from graphql

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb://username:password@ds135068.mlab.com:35068/gql-stock'
+);
+mongoose.connection.once('open', () => {
+  console.log('conected to database');
+});
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
